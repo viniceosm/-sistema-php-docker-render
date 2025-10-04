255452d6ef5bb432562be9e1bb6cad5fbff7d1a8
Vinícius Miiller Rebello <miiller.vini@gmail.com>
2025-10-04 14:44:43 -0300

    Update conexao.php

--- src/conexao.php
+++ src/conexao.php
@@ -1,10 +1,11 @@
 <?php
-$host = "db";                   // nome do serviço no docker-compose
+$host = getenv("MYSQL_HOST");
 $user = getenv("MYSQL_USER");
 $pass = getenv("MYSQL_PASSWORD");
 $db   = getenv("MYSQL_DATABASE");
+$port = getenv("MYSQL_PORT") ?: 3306;
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
